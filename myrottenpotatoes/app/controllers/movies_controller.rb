496bc67f11f38efbd519a5d0b6ce64d981c5a1e8
anne <liann1990@gmail.com>
2012-12-10 09:05:47 +0800

    assignment7

--- myrottenpotatoes/app/controllers/movies_controller.rb
+++ myrottenpotatoes/app/controllers/movies_controller.rb
@@ -13,21 +13,40 @@ class MoviesController < ApplicationController
     # default: render 'new' template
   end
 
+#  def create
+#    @movie = Movie.create!(params[:movie])
+#    flash[:notice] = "#{@movie.title} was successfully created."
+#    redirect_to movies_path
+#  end
   def create
-    @movie = Movie.create!(params[:movie])
-    flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
+    @movie = Movie.new(params[:movie])
+    if @movie.save
+      flash[:notice] = "#{@movie.title} was successfully created."
+      redirect_to movies_path
+    else
+      render 'new' # note, 'new' template can access @movie's field values!
+    end
   end
+
 
   def edit
     @movie = Movie.find params[:id]
   end
  
+#  def update
+#    @movie = Movie.find params[:id]
+#    @movie.update_attributes!(params[:movie])
+#    flash[:notice] = "#{@movie.title} was successfully updated."
+#    redirect_to movie_path(@movie)
+#  end
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(params[:movie])
-    flash[:notice] = "#{@movie.title} was successfully updated."
-    redirect_to movie_path(@movie)
+    if @movie.update_attributes(params[:movie])
+      flash[:notice] = "#{@movie.title} was successfully updated."
+      redirect_to movie_path(@movie)
+    else
+      render 'edit' # note, 'edit' template can access @movie's field values!
+    end
   end
  
   def destroy
--- myrottenpotatoes/app/models/movie.rb
+++ myrottenpotatoes/app/models/movie.rb
@@ -1,2 +1,18 @@
 class Movie < ActiveRecord::Base
-end
+  RATINGS = %w[G PG PG-13 R NC-17]  #  %w[] shortcut for array of strings
+  validates :title, :presence => true
+#  validates :release_date, :presence => true
+  validate :released_1930_or_later # uses custom validator below
+  validates :rating, :inclusion => {:in => RATINGS}
+#    errors.add(:release_date, 'must be 1930 or later') if
+#      self.release_date < Date.parse('1 Jan 1930')
+#  end    
+#end
+
+#  before_save :capitalize_title
+#  def capitalize_title
+#    self.title = self.title.split(/\s+/).map(&:downcase).
+#      map(&:capitalize).join(' ')
+  end
+#end
+
